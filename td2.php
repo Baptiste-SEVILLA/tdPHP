ef2b70be1e27950295b14990f6285e222850ed1e
baptistesevilla <baptiste.sevilla@etu.univ-amu.fr>
2019-09-25 08:32:09 +0200

    Michel

--- td2.php
+++ td2.php
@@ -4,18 +4,13 @@ function start_page($title)
     echo '<!DOCTYPE html> <html 
         lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL;
 };
-?>
 
-<?php
 start_page('test');
 ?>
-
-
 <hr/><br/><strong>Test</strong><br/><hr/>
 <?php
 echo date ('l F d ,Y');
-$jour = date('d/m/Y',strtotime('2020-04-0 1'));
-echo 'Michel';
+$jour = date('d/m/Y',strtotime('2020-04-01'));
 ?>
 
 
